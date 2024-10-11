58bcacdda35bbe63d75b107b7cf78c3db972b282
Slayer <psowa@MSI>
2024-10-11 14:56:44 +0200

    xor encryption and decryption function created and injected to case 3 and 4

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@
 using System.Diagnostics;
 using System.IO;
 using System.IO.Enumeration;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Xml;
 
@@ -66,6 +67,37 @@ class cesar_encryption
     /// <param name="sourcefilename">file to process</param>
     /// <param name="key">cesar key value</param>
     /// <param name="encrypt">set as true for ecryption false for decryption</param>
+    static void data_xor_encrypt_or_decrypt(string sourcefilename, int key, bool encrypt)
+    {
+        string destinationfilename = "";
+        if (encrypt)
+        {
+            destinationfilename = sourcefilename.Replace(".txt", ".xor.txt");
+        }
+        else
+        {
+            destinationfilename = sourcefilename.Replace(".xor.txt", ".txt");
+        }
+        using (StreamReader sourcefile = new StreamReader(sourcefilename))
+        {
+            using (StreamWriter destinationfile = new StreamWriter(destinationfilename))
+            {
+                string readline;
+                while ((readline = sourcefile.ReadLine()) != null)
+                {
+                    StringBuilder writeline = new StringBuilder();
+                    foreach (char letter in readline)
+                    {
+                        char writeletter = (char)(letter ^ key);
+                        writeline.Append(writeletter.ToString());
+
+                    }
+                    destinationfile.WriteLine(writeline);
+                }
+            }
+        }
+    }
+
     static void data_encrypt_or_decrypt(string sourcefilename, int key, bool encrypt)
     {
         string destinationfilename = "";
@@ -122,7 +154,7 @@ class cesar_encryption
         {
             main_menu_show();
 
-            int wybor = int.Parse(Console.ReadLine());   //Wybor uzytkownika 1-3
+            int wybor = int.Parse(Console.ReadLine());   //Wybor uzytkownika 1-5
             Console.Clear();
             string path = "C:\\Users\\psowa\\OneDrive\\Desktop\\Do zaszyfrowania\\";
             int key = 0;
@@ -212,7 +244,6 @@ class cesar_encryption
                         }
 
                         is_validate = false;
-
                         for (int i = 0; i < 3; i++)
                         {
                             Console.Write("Podaj wartość klucza: ");
@@ -234,14 +265,101 @@ class cesar_encryption
                         }
                         break;
                     }
-                case 3:
+                case 3: //decryption xor
                     {
+                        for (int i = 0; i < 3; i++)
+                        {
+                            Console.Write("Podaj nazwę pliku xor: ");
+                            fileName = Console.ReadLine();  // Zapisanie nazwy pliku w pamieci
+                            fileName = Path.Combine(path + fileName);  //Sciezka pliku
+                            is_validate = fileName.Contains(".xor.txt");
 
+                            if (is_validate)
+                            {
+                                if (File.Exists(fileName))
+                                {
+                                    break;
+                                }
+                                is_validate = false;
+                                Console.WriteLine("Plik nie istnieje");
+                                continue;
+                            }
+                            Console.WriteLine("Podaj prawidlowe rozszerzenie!");
+                        }
+
+                        if (!is_validate)
+                        {
+                            break;
+                        }
+
+                        is_validate = false;
+
+                        for (int i = 0; i < 3; i++)
+                        {
+                            Console.Write("Podaj wartość klucza: ");
+                            key = int.Parse(Console.ReadLine());     //Zapisanie klucza w pamieci
+
+                            if (key > 0 && key < 26)
+                            {
+                                is_validate = true;
+                                break;
+                            }
+                            Console.WriteLine("Podaj wartosc klucza od 1-25!");
+                        }
+                        if (is_validate)
+                        {
+                            data_xor_encrypt_or_decrypt(fileName, key, false);
+                        }
                         break;
                     }
-                case 4:
+                case 4: //encryption xor
                     {
-                        
+                        for (int i = 0; i < 3; i++)
+                        {
+                            Console.Write("Podaj nazwę pliku: ");
+                            fileName = Console.ReadLine();  // Zapisanie nazwy pliku w pamieci
+                            fileName = Path.Combine(path + fileName);  //Sciezka pliku
+                            is_validate = fileName.Contains(".txt") && !fileName.Contains(".xor.txt");
+                            is_validate = !fileName.Contains(".enc.txt");
+
+
+                            if (is_validate)
+                            {
+                                if (File.Exists(fileName))
+                                {
+                                    break;
+                                }
+                                is_validate = false;
+                                Console.WriteLine("Plik nie istnieje");
+                                continue;
+
+                            }
+                            Console.WriteLine("Podaj prawidlowe rozszerzenie!");
+                        }
+
+                        if (!is_validate)
+                        {
+                            break;
+                        }
+
+                        is_validate = false;
+
+                        for (int i = 0; i < 3; i++)
+                        {
+                            Console.Write("Podaj wartość klucza: ");
+                            key = int.Parse(Console.ReadLine());     //Zapisanie klucza w pamieci
+
+                            if (key > 0 && key < 26)
+                            {
+                                is_validate = true;
+                                break;
+                            }
+                            Console.WriteLine("Podaj wartosc klucza od 1-25!");
+                        }
+                        if (is_validate)
+                        {
+                            data_xor_encrypt_or_decrypt(fileName, key, true);
+                        }
                         break;
                     }
                 case 5:
