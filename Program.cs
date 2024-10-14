2cf9efc08017efc7d560d2eeeb72a81f7ec4c1d1
Slayer <psowa@MSI>
2024-10-14 10:09:23 +0200

    Add new class XorEncryptOrDecrypt to use in main program

--- Program.cs
+++ Program.cs
@@ -14,45 +14,6 @@ namespace CesarCipher
 {
     class CesarEncryption
     {      
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="sourceFileName">file to process</param>
-        /// <param name="key">cesar key value</param>
-        /// <param name="encrypt">set as true for ecryption false for decryption</param>
-        static void DataXorEncryptOrDecrypt(string sourceFileName, int key, bool encrypt)
-        {
-            string destinationFileName = "";
-            if (encrypt)
-            {
-                destinationFileName = sourceFileName.Replace(".txt", ".xor.txt");
-            }
-            else
-            {
-                destinationFileName = sourceFileName.Replace(".xor.txt", ".txt");
-            }
-            using (StreamReader sourceFile = new(sourceFileName))
-            {
-                using (StreamWriter destinationFile = new(destinationFileName))
-                {
-                    string readLine;
-                    while ((readLine = sourceFile.ReadLine()) != null)
-                    {
-                        StringBuilder writeLine = new();
-                        foreach (char letter in readLine)
-                        {
-                            char writeLetter = (char)(letter ^ key);
-                            writeLine.Append(writeLetter.ToString());
-
-                        }
-                        destinationFile.WriteLine(writeLine);
-                    }
-                }
-            }
-        }
-
-
-
         public static void Main(string[] args)
         {
 
@@ -214,7 +175,7 @@ namespace CesarCipher
                             }
                             if (isValidate)
                             {
-                                DataXorEncryptOrDecrypt(fileName, key, false);
+                                XorEncryptOrDecrypt.DataXorEncryptOrDecrypt(fileName, key, false);
                             }
                             break;
                         }
@@ -264,7 +225,7 @@ namespace CesarCipher
                             }
                             if (isValidate)
                             {
-                                DataXorEncryptOrDecrypt(fileName, key, true);
+                                XorEncryptOrDecrypt.DataXorEncryptOrDecrypt(fileName, key, true);
                             }
                             break;
                         }
--- XorEncryptOrDecrypt.cs
+++ XorEncryptOrDecrypt.cs
@@ -12,21 +12,19 @@ namespace Cesar_Cipher
     /// <param name="sourceFileName">file to process</param>
     /// <param name="key">cesar key value</param>
     /// <param name="encrypt">set as true for ecryption false for decryption</param>
-    public static class EncryptOrDecrypt
+    public static class XorEncryptOrDecrypt
     {
-        public static void DataEncryptOrDecrypt(string sourceFileName, int key, bool encrypt)
+        public static void DataXorEncryptOrDecrypt(string sourceFileName, int key, bool encrypt)
         {
             string destinationFileName = "";
             if (encrypt)
             {
-                destinationFileName = sourceFileName.Replace(".txt", ".enc.txt");
+                destinationFileName = sourceFileName.Replace(".txt", ".xor.txt");
             }
             else
             {
-                destinationFileName = sourceFileName.Replace(".enc.txt", ".txt");
-                key *= -1;
+                destinationFileName = sourceFileName.Replace(".xor.txt", ".txt");
             }
-
             using (StreamReader sourceFile = new(sourceFileName))
             {
                 using (StreamWriter destinationFile = new(destinationFileName))
@@ -37,26 +35,14 @@ namespace Cesar_Cipher
                         StringBuilder writeLine = new();
                         foreach (char letter in readLine)
                         {
-                            char writeLetter = letter;
-                            if (letter >= 'a' && letter <= 'z')
-                            {
-                                char v = (char)((((letter - 'a' + key + 26) % 26) + 'a'));
-                                writeLetter = (char)v;
-                            }
-                            else if (letter >= 'A' && letter <= 'Z')
-                            {
-                                char v = (char)((((letter - 'A' + key + 26) % 26) + 'A'));
-                                writeLetter = (char)v;
-                            }
+                            char writeLetter = (char)(letter ^ key);
                             writeLine.Append(writeLetter.ToString());
+
                         }
                         destinationFile.WriteLine(writeLine);
                     }
-                    destinationFile.Close();
-                    sourceFile.Close();
                 }
             }
-
         }
     }
 }
