cf836822c4778c82f5e10fc4b41c611da47b6080
Slayer <psowa@MSI>
2024-10-14 11:01:40 +0200

    Refactory class menu. Adds automatically prefix and postfix in menu

--- Menu.cs
+++ Menu.cs
@@ -6,54 +6,41 @@ using System.Threading.Tasks;
 
 namespace Cesar_Cipher
 {
-   public static class Menu
+    class Menu
     {
+        private static string prefix = "** ";
+        private static string postfix = " **";
+
+        private const int menuLength = 40; //Length of menu
+
         public static void MainMenuShow()
         {
             int tempo = 1;
 
-            string system0 = "** Cesar Cipher                     **";
-            string system1 = "** 1. Open file ot decrypt.         **";
-            string system2 = "** 2. Open file to encrypt.         **";
-            string system3 = "** 3. Decrypting xor                **";
-            string system4 = "** 4. Encrypting xor                **";
-            string system5 = "** 5. Exit                          **";
+            string system0 = "Cesar Cipher";
+            string system1 = "1. Otwórz plik do deszyfrowania.";
+            string system2 = "2. Otwórz plik do szyfrowania.";
+            string system3 = "3. Deszyfrowanie xor";
+            string system4 = "4. Szyfrowanie xor";
+            string system5 = "5. Exit";
 
             Console.Clear();
-            foreach (char letters_system in system0) //System0
+
+            PrintMenuLine(system0);
+            PrintMenuLine(system1);
+            PrintMenuLine(system2);
+            PrintMenuLine(system3);
+            PrintMenuLine(system4);
+            PrintMenuLine(system5);
+        }
+
+        private static void PrintMenuLine(string text)
+        {
+            string formattedText = prefix + text.PadRight(menuLength - prefix.Length - postfix.Length) + postfix;
+            foreach (char letter in formattedText)
             {
-                Console.Write(letters_system);
-                Thread.Sleep(tempo);
-            }
-            Console.WriteLine();
-            foreach (char letters_user in system1) //System1
-            {
-                Console.Write(letters_user);
-                Thread.Sleep(tempo);
-            }
-            Console.WriteLine();
-            foreach (char letters_system in system2) //System2
-            {
-                Console.Write(letters_system);
-                Thread.Sleep(tempo);
-            }
-            Console.WriteLine();
-            foreach (char letters_user in system3) //System3
-            {
-                Console.Write(letters_user);
-                Thread.Sleep(tempo);
-            }
-            Console.WriteLine();
-            foreach (char letters_user in system4) //System4
-            {
-                Console.Write(letters_user);
-                Thread.Sleep(tempo);
-            }
-            Console.WriteLine();
-            foreach (char letters_user in system5) //System5
-            {
-                Console.Write(letters_user);
-                Thread.Sleep(tempo);
+                Console.Write(letter);
+                Thread.Sleep(1);
             }
             Console.WriteLine();
         }
