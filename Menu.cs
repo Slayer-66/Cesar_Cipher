b75a8410b8865e068c425be7465324c1c4301207
Slayer <psowa@MSI>
2024-10-14 12:38:27 +0200

    Changed menu code, now it is possible to automatically push added text on the screen

--- Menu.cs
+++ Menu.cs
@@ -11,32 +11,30 @@ namespace Cesar_Cipher
         private static string prefix = "** ";
         private static string postfix = " **";
 
-        private const int menuLength = 40; //Length of menu
+        private const int menuLength = 40;
 
         public static void MainMenuShow()
         {
-            int tempo = 1;
-
-            string system0 = "Cesar Cipher";
-            string system1 = "1. Otwórz plik do deszyfrowania.";
-            string system2 = "2. Otwórz plik do szyfrowania.";
-            string system3 = "3. Deszyfrowanie xor";
-            string system4 = "4. Szyfrowanie xor";
-            string system5 = "5. Exit";
-
+            string[] menuItems = new string[]
+            {
+                "Cesar Cipher",
+                "1. Otwórz plik do deszyfrowania.",
+                "2. Otwórz plik do szyfrowania.",
+                "3. Deszyfrowanie xor",
+                "4. Szyfrowanie xor",
+                "5. Exit",
+            };
             Console.Clear();
 
-            PrintMenuLine(system0);
-            PrintMenuLine(system1);
-            PrintMenuLine(system2);
-            PrintMenuLine(system3);
-            PrintMenuLine(system4);
-            PrintMenuLine(system5);
+            foreach (var item in menuItems)
+            {
+                PrintMenuLine(item);
+            }
         }
-
         private static void PrintMenuLine(string text)
         {
-            string formattedText = prefix + text.PadRight(menuLength - prefix.Length - postfix.Length) + postfix;
+            string formattedText = string.Format("{0}{1}{2}", prefix, text.PadRight(menuLength - prefix.Length - postfix.Length), postfix);
+
             foreach (char letter in formattedText)
             {
                 Console.Write(letter);
