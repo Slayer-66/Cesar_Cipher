b4dd084ce157d7273092f526d82387c4114457d5
Slayer <psowa@MSI>
2024-10-15 12:08:12 +0200

    More readable code in program.cs. Now program contains methods intended to for example checking a validate of keys or file

--- Program.cs
+++ Program.cs
@@ -1,233 +1,127 @@
 ﻿using Cesar_Cipher;
 using System;
-using System.ComponentModel.Design;
-using System.Diagnostics;
 using System.IO;
-using System.IO.Enumeration;
-using System.Runtime.InteropServices;
-using System.Text;
-using System.Xml;
+using System.Threading;
 
 namespace Cesar_Cipher
 {
     class CesarEncryption
     {
+        private static string path = "C:\\Users\\psowa\\OneDrive\\Desktop\\Do zaszyfrowania\\"; //path
         public static void Main(string[] args)
         {
             do
             {
-                Menu.MainMenuShow();
+                Menu.MainMenuShow(); //menu method
 
-                int choice = int.Parse(Console.ReadLine());   //Users choice 1-5
+                int choice = InputHelper.GetValidChoice();
                 Console.Clear();
-                string path = "C:\\Users\\psowa\\OneDrive\\Desktop\\Do zaszyfrowania\\";
-                int key = 0;
-                bool isValidate = false;
-                string fileName = "";
 
                 switch (choice)
                 {
-                    case 1:///decryption
-                        {
-                            for (int i = 0; i < 3; i++)
-                            {
-                                Console.Write("Type file name: ");
-                                fileName = Console.ReadLine();  // Saving fileName in computer memory
-                                fileName = Path.Combine(path + fileName);  //files path
-                                isValidate = fileName.Contains(".enc.txt");  //checking if file name contains correct extension
-
-                                if (isValidate)
-                                {
-                                    if (File.Exists(fileName))
-                                    {
-                                        break;
-                                    }
-                                    isValidate = false;
-                                    Console.WriteLine("File doesn't exist");
-                                    continue;
-                                }
-                                Console.WriteLine("Uncorrect extension");
-                            }
-                            if (!isValidate)
-                            {
-                                break;
-                            }
-                            isValidate = false;
-
-                            for (int i = 0; i < 3; i++)
-                            {
-                                Console.WriteLine();
-                                Console.Write("Type value of the key: ");
-                                key = int.Parse(Console.ReadLine());     //Saving key in memory
-
-                                if (key > 0 && key < 26)
-                                {
-                                    isValidate = true;
-                                    break;
-                                }
-                                Console.Write("Type value of the key between 1-25");
-                            }
-                            if (isValidate)
-                            {
-                                CesarCipher cesarCipher = new();
-                                cesarCipher.EncryptOrDecrypt(fileName, key, false);
-                                Console.WriteLine("Thank you");
-                                Thread.Sleep(1000);
-                            }
-                            break;
-                        }
-                    case 2://encryption
-                        {
-                            for (int i = 0; i < 3; i++)
-                            {
-                                Console.Write("Type file name: ");
-                                fileName = Console.ReadLine();  //Saving fileName in computer memory
-                                fileName = Path.Combine(path + fileName);  //Files path
-                                isValidate = fileName.Contains(".txt") && !fileName.Contains(".enc.txt");
-
-                                if (isValidate)
-                                {
-                                    if (File.Exists(fileName))
-                                    {
-                                        break;
-                                    }
-                                    isValidate = false;
-                                    Console.WriteLine("File doesn't exist");
-                                    continue;
-                                }
-                                Console.WriteLine("Uncorrect extension");
-                            }
-                            if (!isValidate)
-                            {
-                                break;
-                            }
-                            isValidate = false;
-                            for (int i = 0; i < 3; i++)
-                            {
-                                Console.Write("Type value of the key: ");
-                                key = int.Parse(Console.ReadLine());     //Saving key in memory
-
-                                if (key > 0 && key < 26)
-                                {
-                                    isValidate = true;
-                                    break;
-                                }
-                                Console.WriteLine("Type value of the key between 1-25!");
-                            }
-                            if (isValidate)
-                            {
-                                CesarCipher cesarCipher = new();
-                                cesarCipher.EncryptOrDecrypt(fileName, key, true);
-                                Console.WriteLine("Thank you");
-                                Thread.Sleep(1000);
-                            }
-                            break;
-                        }
-                    case 3: //decryption xor
-                        {
-                            for (int i = 0; i < 3; i++)
-                            {
-                                Console.Write("Type xor file name: ");
-                                fileName = Console.ReadLine();  //Saving fileName in computer memory
-                                fileName = Path.Combine(path + fileName);  //Files name
-                                isValidate = fileName.Contains(".xor.txt");
-
-                                if (isValidate)
-                                {
-                                    if (File.Exists(fileName))
-                                    {
-                                        break;
-                                    }
-                                    isValidate = false;
-                                    Console.WriteLine("File doesn't exist");
-                                    continue;
-                                }
-                                Console.WriteLine("Uncorrect extension");
-                            }
-                            if (!isValidate)
-                            {
-                                break;
-                            }
-                            isValidate = false;
-
-                            for (int i = 0; i < 3; i++)
-                            {
-                                Console.Write("Type of the key: ");
-                                key = int.Parse(Console.ReadLine());     //Saving key in memory
-
-                                if (key > 0 && key < 26)
-                                {
-                                    isValidate = true;
-                                    break;
-                                }
-                                Console.WriteLine("Type value of the key between 1-25!");
-                            }
-                            if (isValidate)
-                            {
-                                XorEncrypt xorEncrypt = new();
-                                xorEncrypt.EncryptOrDecrypt(fileName, key, false);
-                            }
-                            break;
-                        }
-                    case 4: //encryption xor
-                        {
-                            for (int i = 0; i < 3; i++)
-                            {
-                                Console.Write("Type file name: ");
-                                fileName = Console.ReadLine();  //Saving fileName in computer memory
-                                fileName = Path.Combine(path + fileName);  //Files path
-                                isValidate = fileName.Contains(".txt") && !fileName.Contains(".xor.txt");
-                                isValidate = !fileName.Contains(".enc.txt");
-
-                                if (isValidate)
-                                {
-                                    if (File.Exists(fileName))
-                                    {
-                                        break;
-                                    }
-                                    isValidate = false;
-                                    Console.WriteLine("File doesn't exist");
-                                    continue;
-                                }
-                                Console.WriteLine("Uncorrect extension");
-                            }
-                            if (!isValidate)
-                            {
-                                break;
-                            }
-                            isValidate = false;
-
-                            for (int i = 0; i < 3; i++)
-                            {
-                                Console.Write("Type value of the key: ");
-                                key = int.Parse(Console.ReadLine());     //Saving key in memory
-
-                                if (key > 0 && key < 26)
-                                {
-                                    isValidate = true;
-                                    break;
-                                }
-                                Console.WriteLine("Type value of the key between 1-25!");
-                            }
-                            if (isValidate)
-                            {
-                                XorEncrypt xorEncrypt = new();
-                                xorEncrypt.EncryptOrDecrypt(fileName, key, true);
-                            }
-                            break;
-                        }
-                    case 5:
-                        {
-                            Environment.Exit(0);
-                            break;
-                        }
+                    case 1: // Decryption 
+                        ProcessCesarEncryption(false);
+                        break;
+                    case 2: // Encryption 
+                        ProcessCesarEncryption(true);
+                        break;
+                    case 3: // Decryption xor
+                        ProcessXorEncryption(false);
+                        break;
+                    case 4: // Encryption xor
+                        ProcessXorEncryption(true);
+                        break;
+                    case 5: // Exit
+                        Environment.Exit(0);
+                        break;
                     default:
-                        {
-                            Console.Write("Uncorrect option");
-                            break;
-                        }
+                        Console.WriteLine("Uncorrect option");
+                        break;
                 }
             } while (true);
+        }
+        private static void ProcessCesarEncryption(bool encrypt)
+        {
+            string fileName = FileHelper.GetValidFileName(encrypt ? ".txt" : ".enc.txt", !encrypt);
+            if (fileName == null) return;
+
+            int key = KeyHelper.GetValidKey();
+            if (key == -1) return;
+
+            CesarCipher cesarCipher = new();
+            cesarCipher.EncryptOrDecrypt(fileName, key, encrypt);
+
+            Console.WriteLine("Operation completed.");
+            Thread.Sleep(1000);
+        }
+        private static void ProcessXorEncryption(bool encrypt)
+        {
+            string fileName = FileHelper.GetValidFileName(encrypt ? ".txt" : ".xor.txt", !encrypt);
+            if (fileName == null) return;
+
+            int key = KeyHelper.GetValidKey();
+            if (key == -1) return;
+
+            XorEncrypt xorEncrypt = new();
+            xorEncrypt.EncryptOrDecrypt(fileName, key, encrypt);
+
+            Console.WriteLine("Operation completed.");
+            Thread.Sleep(1000);
+        }
+    }
+    public static class FileHelper
+    {
+        private static string path = "C:\\Users\\psowa\\OneDrive\\Desktop\\Do zaszyfrowania\\";
+
+          //checking if user typed valid type of file and extension
+        public static string GetValidFileName(string expectedExtension, bool shouldExist)
+        {
+            string fileName;
+            bool isValidate = false;
+
+            for (int i = 0; i < 3; i++)
+            {
+                Console.Write("Type file name: ");
+                fileName = Console.ReadLine();
+                fileName = Path.Combine(path + fileName);
+
+                isValidate = fileName.Contains(expectedExtension) && File.Exists(fileName) == shouldExist;
+                if (isValidate) return fileName;
+
+                Console.WriteLine("File doesn't exist or incorrect extension.");
+            }
+            return null;
+        }
+    }
+      //cheching if user typed a valid choice
+    public static class KeyHelper
+    {
+        public static int GetValidKey()
+        {
+            int key = 0;
+            for (int i = 0; i < 3; i++)
+            {
+                Console.Write("Type a value of the key (1-25): ");
+                if (int.TryParse(Console.ReadLine(), out key) && key > 0 && key < 26)
+                {
+                    return key;
+                }
+                Console.WriteLine("Please enter a value between 1 and 25.");
+            }
+            return -1;
+        }
+    }
+     //cheching if user typed a valid choice
+    public static class InputHelper
+    {
+        public static int GetValidChoice()
+        {
+            int choice;
+            while (!int.TryParse(Console.ReadLine(), out choice) || choice < 1 || choice > 5)
+            {
+                Console.WriteLine("Uncorrect option");
+            }
+            return choice;
         }
     }
 }
